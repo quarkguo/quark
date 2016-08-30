0aaa3adf0e7a2c5e4cc5f9370df08fc64dd1971a
quark guo <henguo@gmail.com>
2016-08-30 00:01:02 -0500

    commented out highlight pdf content

--- ccgrest/WebContent/javascript/ccg/ccg_document.js
+++ ccgrest/WebContent/javascript/ccg/ccg_document.js
@@ -68,11 +68,11 @@ ccg.ui.updateSelectedContent= function(data,searchKey){
 	 	// rendering PDF document
 		Ext.getCmp('contenttabpanel').setActiveTab(0);
 		var url='rest/article/'+data.articleID+'/'+data.startPage+'-'+data.endPage+'/download'
-		if(searchKey!=null)
+		/*if(searchKey!=null)
 		{
 			var query=escape(searchKey);
 			url='rest/article/'+data.articleID+'/'+data.startPage+'-'+data.endPage+'/'+query+'/download';
-		}
+		}*/
 	 	var pdfPanel=document.getElementById('pdfcontent');
 	 	pdfPanel.src=url;
 	 	
