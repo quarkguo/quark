1fda1d7757a08f9c24e90af0b3a8a03f64930b39
Zhongkai Chen <zchen323@Xis-MacBook-Pro-2.local>
2016-09-29 00:19:43 -0500

    update

--- ingestion/src/main/java/com/ccg/ingestion/extract/CategoryRegexPattern.java
+++ ingestion/src/main/java/com/ccg/ingestion/extract/CategoryRegexPattern.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ccg.ingestion.extract;
 
 public class CategoryRegexPattern {
@@ -66,72 +65,3 @@ public class CategoryRegexPattern {
 		
 	}
 }
-=======
-package com.ccg.ingestion.extract;
-
-public class CategoryRegexPattern {
-	
-	private String root="(\\nExecutive+\\sSummary)|(\\n\\d(1,3)+\\xA0{0,3}\\s{1,4}+[\\D\\d])|(\\nAppendix)";
-	private String head="\\n\\d+";
-	private String tail="\\xA0{0,3}\\s{1,4}+\\w";
-	private String ext="\\.[0-9A-H]+";
-
-	final public static String[] _ROOT_LIST_={
-			"(\\nExecutive+\\sSummary)|(\\n\\d{1,3}+\\xA0{0,3}\\s{1,4}+[\\D\\d])|(\\nAppendix)|(\\nFigure\\s\\d+\\:)|(\\nTable\\s\\d+\\:)",
-			"(\\nExecutive+\\sSummary)|(\\n\\d{1,3}+\\.0\\xA0{0,3}\\s{1,4}+[\\D\\d])|(\\nAppendix)|(\\nFigure\\s\\d+\\:)|(\\nTable\\s\\d+\\:)",
-			"(\\nExecutive+\\sSummary)|(\\r?\\n\\d{1,3}+\\.\\xA0{0,3}\\s{1,4}+[\\D\\d])|(\\nAppendix)|(\\nFigure\\s\\d+\\:)|(\\nTable\\s\\d+\\:)"
-	};
-
-	public String getRoot() {
-		return root;
-	}
-
-	public void setRoot(String root) {
-		this.root = root;
-	}
-
-	public String getHead() {
-		return head;
-	}
-
-	public void setHead(String head) {
-		this.head = head;
-	}
-
-	public String getTail() {
-		return tail;
-	}
-
-	public void setTail(String tail) {
-		this.tail = tail;
-	}
-
-	public String getExt() {
-		return ext;
-	}
-
-	public void setExt(String ext) {
-		this.ext = ext;
-	}
-	
-	public String getSubCategoryRegex(int level)
-	{
-		if(level>0)
-		{
-			StringBuffer buf=new StringBuffer();
-			buf.append(head);
-			for(int i=0;i<level;i++)
-			{
-				buf.append(ext);
-			}
-			buf.append(tail);
-			return buf.toString();
-		}
-		else
-		{
-			return root;
-		}
-		
-	}
-}
->>>>>>> 9bd2f86d0d6b94f4bb40ade21317876033b21128
